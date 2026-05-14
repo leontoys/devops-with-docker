40f840f02cdba46b3f6de10d0b7d435d44706a46
leontoys <svliyon@gmail.com>
2026-05-15 08:20:39 +1000

    completed docker script

---
+++ 3.4/builder.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# remote repo
+remote_repo="https://github.com/"$1
+# local repo
+local_repo=$(echo "$1" | cut -d'/' -f2)
+# clone remote repo
+git clone $remote_repo
+# move to local repo
+cd $local_repo
+
+# login to Docker Hub using env vars
+echo "$DOCKER_PWD" | docker login -u "$DOCKER_USER" --password-stdin
+
+# build image
+docker build -t $local_repo .
+# tag image
+docker tag $local_repo $2
+# push to docker hub
+docker push $2
+# show message its completed
+echo "done"
