ded090a81aabacbb37fc3d8c6c2c1db7d443b437
leontoys <svliyon@gmail.com>
2026-05-16 05:23:42 +1000

    dockerised thankyou note

---
+++ docker-thanks/src/App.jsx
@@ -0,0 +1,31 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+      <div>
+        <h1>Thank You</h1>
+
+        <p>
+          Thank you the University of Helsinki, Matti Luukkainen 
+          and everyone who reviewed and helped throughout the course.
+        </p>
+
+        <p>
+          I learned a lot from this course and truly enjoyed
+          the journey into Docker and DevOps.
+        </p>
+      </div>
+      </section>
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
